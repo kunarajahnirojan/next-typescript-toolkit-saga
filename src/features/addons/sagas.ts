d9a99ec2d66a6b2a8cf47d4d16b24b005b85b813
Nirojan <kunarajahnirojan@gmail.com>
2024-02-28 15:54:17 +0530

    env changes

--- src/features/addons/sagas.ts
+++ src/features/addons/sagas.ts
@@ -11,12 +11,10 @@ import {
 } from './addonSlice';
 
 function* fetchTitles() {
-  const headers = {
-    Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhZG1pbkBleGFtcGxlLmNvbSIsImp0aSI6Ijk4N2IxYTExLWVkNzYtNGFiYi05YWExLWY2YWY0NDY2NDY3YSIsImVtYWlsIjoiYWRtaW5AZXhhbXBsZS5jb20iLCJ1c2VyTmFtZSI6InN5c2FkbWluIiwiaWQiOiIxIiwibmJmIjoxNjkxNDg5Njg2LCJleHAiOjE2OTM1NjMyODYsImlhdCI6MTY5MTQ4OTY4Nn0.Ey5xa-lWZLrLUEHKjBH52StLm79SOF77kMt8mRfm1pw`,
-  };
+ 
 
   try {
-    const { data } = yield call(axios.get, `${apiURL}title`, { headers });
+    const { data } = yield call(axios.get, `${apiURL}title`);
     yield put(loadTitleSuccess(data));
   } catch (error: any) {
     yield put(loadTitleFail(error.message));
